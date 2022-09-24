3a8f3739643820fda433f7d7e66f5e50c017cc8d
TallTalha <talhaburakaydin.official@gmail.com>
2022-09-24 18:02:08 +0300

    First Video - Hello World

--- Courses and contents/HelloWorld.sol
+++ Courses and contents/HelloWorld.sol
@@ -2,6 +2,10 @@
 // compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
 pragma solidity ^0.8.13;
 
+/*
+First Video 
+*/
+
 contract HelloWorld {
     string private greeting = "Hello World";
 }
